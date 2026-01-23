223a0a96af9a3d0f8c0b28d8e3e16543ccfc3e6d
Junaid-919 <s.md.junaidd@gmail.com>
2026-01-23 18:06:27 +0530

    Update MapView.js

--- src/pages/MapView.js
+++ src/pages/MapView.js
@@ -27,31 +27,35 @@ const MapView = () => {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
 
+  // Get the last record
+  const lastLocation = locations.length ? locations[locations.length - 1] : null;
+
+  if (!lastLocation) return <div>No locations available</div>;
+
+  const position = [parseFloat(lastLocation.latitude), parseFloat(lastLocation.longitude)];
+
   return (
     <div>
-      <h1>Executive Locations</h1>
-      <MapContainer center={[15.8287, 78.0380]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <h1>Executive Location</h1>
+      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        {locations.map((item, index) => (
-          <Marker
-            key={index}
-            position={[parseFloat(item.latitude), parseFloat(item.longitude)]}
-            icon={L.icon({
-              iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-              iconSize: [25, 41],
-              iconAnchor: [12, 41],
-              popupAnchor: [0, -41],
-            })}
-          >
-            <Popup>
-              Latitude: {item.latitude} <br />
-              Longitude: {item.longitude}
-            </Popup>
-          </Marker>
-        ))}
+        <Marker
+          position={position}
+          icon={L.icon({
+            iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [0, -41],
+          })}
+        >
+          <Popup>
+            Latitude: {lastLocation.latitude} <br />
+            Longitude: {lastLocation.longitude}
+          </Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
