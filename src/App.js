406913b2eed42ca067ff715135d4c42516e8af9d
Junaid-919 <s.md.junaidd@gmail.com>
2026-02-19 16:42:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import PersonsList from "./pages/PersonsList";
+import BusStopScreen from "./pages/BusStopScreen";
 import MapView from "./pages/MapView";
 
 function App() {
@@ -11,6 +12,10 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/persons" element={<PersonsList />} />
         <Route path="/map" element={<MapView />} />
+        <Route
+        path="/bus/:bus_stop_number"
+        element={<BusStopScreen />}
+      />
       </Routes>
     </BrowserRouter>
   );
